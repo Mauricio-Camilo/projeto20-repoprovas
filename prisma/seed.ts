99733a465e11f28669a758042f52a0fc74b75e51
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-07-22 20:58:01 -0500

    update: add prisma sea s seed.ts with all data to insert into the tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,65 @@
 import prisma from "./../src/config/database.js";
 
-/* Pra funcionar essa configuração que vai alimentar o banco de dados auxiliar,
-é necessário adicionar no package json o prisma e indicar esse arquivo pra rodas
-ao iniciar o teste
-*/
+const terms = [
+    {number:1},
+    {number:2},
+    {number:3},
+    {number:4},
+    {number:5},
+    {number:6}
+]
 
-//COLOCAR AQUI AS INSERÇÕES MANUIAS PASSADAS NO PROJETO
+const disciplines = [
+    {name: "HTML e CSS", termId: 1},
+    {name: "Javascript", termId: 2},
+    {name: "React", termId: 3},
+    {name: "Humildade", termId: 4},
+    {name: "Planejamento", termId: 5},
+    {name: "Autoconfiança", termId: 6}
+]
+
+const categories = [
+    {name: "Projeto"},
+    {name: "Prática"},
+    {name: "Recuperação"},
+]
+
+const teachers = [
+    {name: "Diego Pinho"},
+    {name: "Bruna Hamori"}
+]
+
+const teachersDisciplines = [
+    {teacherId: 1, disciplineId: 1},
+    {teacherId: 1, disciplineId: 2},
+    {teacherId: 1, disciplineId: 3},
+    {teacherId: 2, disciplineId: 4},
+    {teacherId: 2, disciplineId: 5},
+    {teacherId: 2, disciplineId: 6}
+]   
+
+const disciplinesCategories = [
+    {disciplineId: 1, categoryId: 1},
+    {disciplineId: 1, categoryId: 3},
+    {disciplineId: 4, categoryId: 2},
+    {disciplineId: 4, categoryId: 3}
+]
+
+const teachersCategories = [
+    {teacherId: 1, categoryId: 1},
+    {teacherId: 1, categoryId: 3},
+    {teacherId: 2, categoryId: 2},
+    {teacherId: 2, categoryId: 3}
+]
 
 async function main () {
-    // await prisma.user.create({
-    //     data: {
-    //         email:"emailTeste5@email.com",
-    //         password:"abcde"
-    //     }
-    // })
+    // await prisma.term.createMany({data : terms});
+    // await prisma.discipline.createMany({data : disciplines});
+    // await prisma.category.createMany({data : categories});
+    // await prisma.teacher.createMany({data : teachers});
+    // await prisma.teachersDisciplines.createMany({data : teachersDisciplines});
+    // await prisma.disciplinesCategories.createMany({data : disciplinesCategories});
+    // await prisma.teachersCategories.createMany({data : teachersCategories});
 }
 
 main().catch(e => {
